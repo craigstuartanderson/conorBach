9a77e61a57a62f54265afa06b95f04d54c4a9bb2
Craig Anderson <craigstuartanderson@gmail.com>
2018-02-03 13:14:50 -0500

    first commit

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyAVO9zJf_NAk-Zk3VxATGASubOmsETzSbE",
+  authDomain: "conorbach.firebaseapp.com",
+  databaseURL: "https://conorbach.firebaseio.com",
+  projectId: "conorbach",
+  storageBucket: "conorbach.appspot.com",
+  messagingSenderId: "45959161852"
+};
+firebase.initializeApp(config);
+
+
+
 class App extends React.Component {
     render() {
       return (
         <div>
-          Hello
+          <h1>Conor Holler Bachelor Chalet</h1>
+          <h2>Get ready for the weekend</h2>
         </div>
       )
     }
